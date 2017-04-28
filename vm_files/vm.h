b859bfca1c78191f746793e9adce623e731c529b
Thibaut FONTAINE <tfontain@e1r11p10.42.fr>
2017-04-28 15:44:53 +0200

    adding libft.h to vm.h

--- vm_files/vm.h
+++ vm_files/vm.h
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 20:50:25 by tfontain          #+#    #+#             */
-/*   Updated: 2017/04/27 20:55:42 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/28 15:44:36 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,4 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "../libft/includes/libft.h"
