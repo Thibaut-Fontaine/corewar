28b9a38704b3a941515d4662555c25a63dade3cc
Matthias PERRONCEL <mperronc@student.42.fr>
2017-05-02 20:29:42 +0200

    func pointers are func

--- asm/src/generate_op_from_line.c
+++ asm/src/generate_op_from_line.c
@@ -6,19 +6,23 @@
 /*   By: mperronc <mperronc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/29 16:11:40 by mperronc          #+#    #+#             */
-/*   Updated: 2017/05/02 20:18:40 by mperronc         ###   ########.fr       */
+/*   Updated: 2017/05/02 20:29:27 by mperronc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_asm.h"
 
-size_t	generate_op_from_line(char	*line, t_oplist *oplist, t_labellist *labels)
+size_t	generate_op_from_line(char *line, t_oplist *oplist, t_labellist *labels)
 {
-	size_t	opsize;
-	t_tokens
+	size_t		opsize;
+	t_tokenlist	*tokens;
 
-	get_label_token()
-	get_op_token()
+	tokens = token_init();
+
+
+	if get_token(&extract_label, LABEL)
+		// on a une label
+	get_token(&extract_op, OP)
 	while (get_arg_tokens())
 		get_sep_token()
 	append_endl_token()
@@ -27,3 +31,14 @@ size_t	generate_op_from_line(char	*line, t_oplist *oplist, t_labellist *labels)
 
 	calculate_size(tokenlistlocale)
 }
+
+
+get_token(f, type)
+	token = malloc(blabla);
+	token->content = f(line);
+	if token->content
+		token->type = type
+		return token
+	else
+		free(token)
+		return NULL
