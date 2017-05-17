5b03e2cc7f4e2da8ab636419552c3c62b0a35297
Matthias PERRONCEL <mperronc@student.42.fr>
2017-05-17 18:06:16 +0200

    free name

--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jgagnot <jgagnot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 11:59:31 by jgagnot           #+#    #+#             */
-/*   Updated: 2017/05/17 17:56:01 by mperronc         ###   ########.fr       */
+/*   Updated: 2017/05/17 18:05:59 by mperronc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int		main(int ac, char **av)
 {
 	t_asm		env;
+	char		*name;
 	int			i;
 
 	if (ac != 2)
@@ -23,10 +24,12 @@ int		main(int ac, char **av)
 		ft_putstr(av[0]);
 		ft_putendl(" <sourcefile.s>");
 		return (-1);
-	}		
+	}
 	env = parse(av[1]);
 	i = ft_strlen(av[1]) - 2;
-	generate_bytecode(&env, replace_filename(av[1]));
+	generate_bytecode(&env, name = replace_filename(av[1]));
+	free(name);
 	free_asm(&env);
+	sleep(2000);
 	return (0);
 }
