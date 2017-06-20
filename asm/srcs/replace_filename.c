be31b7c27694cd9895dd6bf5a8323b4129653a41
Matthias PERRONCEL <mperronc@student.42.fr>
2017-06-20 17:10:39 +0200

    Rewrite replace_filename (works properly now)

--- asm/srcs/replace_filename.c
+++ asm/srcs/replace_filename.c
@@ -6,32 +6,54 @@
 /*   By: mperronc <mperronc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/17 17:37:40 by mperronc          #+#    #+#             */
-/*   Updated: 2017/05/17 17:52:40 by mperronc         ###   ########.fr       */
+/*   Updated: 2017/06/20 17:09:50 by mperronc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-char	*replace_filename(char *path)
+static char	*copy_path_upto_slash(char *new, char *path)
 {
-	char	*new;
-	int		i;
+	char *ptr;
 
-	i = 0;
-	new = ft_strnew(ft_strlen(path) + 4);
-	if (!ft_strchr(path, '.'))
+	ptr = path + ft_strlen(path) - 1;
+	while (*ptr != '/')
+		ptr--;
+	ft_strncpy(new, path, ptr - path);
+	return (ptr);
+}
+
+static void	replace_extension(char *new, char *path)
+{
+	char *ptr;
+
+	if (ft_strchr(path, '.'))
 	{
-		new = ft_strcpy(new, path);
+		ptr = path + ft_strlen(path) - 1;
+		while (*ptr != '.')
+			ptr--;
+		ft_strncpy(new, path, ptr - path);
 		ft_strcat(new, ".cor");
 	}
 	else
 	{
-		while (path[i] != '.')
-		{
-			new[i] = path[i];
-			i++;
-		}
+		ft_strcat(new, path);
 		ft_strcat(new, ".cor");
 	}
+}
+
+char		*replace_filename(char *path)
+{
+	char *new;
+	char *ptr;
+
+	new = ft_strnew(ft_strlen(path) + 4);
+	if (ft_strchr(path, '/'))
+	{
+		ptr = copy_path_upto_slash(new, path);
+		replace_extension(new + ft_strlen(new), ptr);
+	}
+	else
+		replace_extension(new, path);
 	return (new);
 }
