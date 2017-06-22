5da96a93e2a717c17d2e004d6877957be8b9fb91
Jeremy GAGNOT <jgagnot@e2r13p9.42.fr>
2017-06-22 18:23:52 +0200

    need to fix store_at.c

--- vm_files/instructions_jgagnot.c
+++ vm_files/instructions_jgagnot.c
@@ -21,16 +21,16 @@ int		is_valid_reg(int reg)
 
 int 	store_at(char *arena, int i, char *val)
 {
-	*arena + (i % MEM_SIZE) = *val;
-	*arena + ((i + 1) % MEM_SIZE) = *(val + 1);
-	*arena + ((i + 2) % MEM_SIZE) = *(val + 2);
-	*arena + ((i + 3) % MEM_SIZE) = *(val + 3);
+	*(arena + (i % MEM_SIZE)) = *val;
+	*(arena + ((i + 1) % MEM_SIZE)) = *(val + 1);
+	*(arena + ((i + 2) % MEM_SIZE)) = *(val + 2);
+	*(arena + ((i + 3) % MEM_SIZE)) = *(val + 3);
 	return (0);
 }
 
-int		st(t_proc *proc, t_instruct *instruct, char *arena)
+int		st(t_process *proc, t_instruct *instruct, char *arena)
 {
-	int		val;
+	char	*val;
 
 	if (!is_valid_reg(instruct->args[0]))
 		return (0);
@@ -42,15 +42,15 @@ int		st(t_proc *proc, t_instruct *instruct, char *arena)
 		proc->reg[instruct->args[1]] = val;
 	}
 	else
-		store_at(arena, proc->pc + (instruct->args[1] % IDX_MOD), (char*)val);
-	if (val == 0)
+		store_at(arena, proc->pc + (instruct->args[1] % IDX_MOD), val);
+	if ((int)val == 0)
 		proc.carry = 1;
 	else
 		proc.carry = 0;
 	return (1);
 }
 
-int		add(t_proc *proc, t_instruct *instruct)
+int		add(t_process *proc, t_instruct *instruct)
 {
 	if (!is_valid_reg(instruct->args[0]) || !is_valid_reg(instruct->args[1])
 		|| !is_valid_reg(instruct->args[2]))
@@ -64,7 +64,7 @@ int		add(t_proc *proc, t_instruct *instruct)
 	return (1);
 }
 
-int		sub(t_proc *proc, t_instruct *instruct)
+int		sub(t_process *proc, t_instruct *instruct)
 {
 	if (!is_valid_reg(instruct->args[0]) || !is_valid_reg(instruct->args[1])
 		|| !is_valid_reg(instruct->args[2]))
