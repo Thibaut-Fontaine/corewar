6732677826ea763075ea747838959d5bd58a098f
Thibaut FONTAINE <tfontain@e2r12p11.42.fr>
2017-06-23 06:24:37 +0200

    and, or, xor et zjmp termine. les autres instructions sont censees modifier le carry mais ne le font pas encore

--- vm_files/srcs/instructions_tfontain.c
+++ vm_files/srcs/instructions_tfontain.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <tfontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 01:26:41 by tfontain          #+#    #+#             */
-/*   Updated: 2017/06/22 19:29:27 by mperronc         ###   ########.fr       */
+/*   Updated: 2017/06/23 06:23:33 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int					op_value(t_process *proc, t_instruct *i, char *arena, int n)
 	{
 		if (i->args[n] > 16 || i->args[n] < 1)
 			return (0);
-		return (*(int*)(proc->reg[i->args[n] - 1]));
+		return (proc->reg[i->args[n] - 1]);
 	}
 	if (i->types[n] == T_IND)
 		return (*(arena + ((i->args[n] - 1 + proc->pc) % MEM_SIZE)));
@@ -39,7 +39,7 @@ void				*op_stock(t_process *proc, t_instruct *i, char *arena, int n)
 	{
 		if (i->args[n] > 16 || i->args[n] < 1)
 			return (NULL);
-		return (proc->reg[i->args[n] - 1]);
+		return (proc->reg + i->args[n] - 1);
 	}
 	if (i->types[n] == T_IND)
 		return (arena + ((i->args[n] - 1 + proc->pc) % MEM_SIZE));
@@ -50,50 +50,37 @@ void				*op_stock(t_process *proc, t_instruct *i, char *arena, int n)
 
 int					and(t_process *proc, t_instruct *i, char *arena)
 {
-	*(int*)op_stock(proc, i, arena, 3) =
-		(op_value(proc, i, arena, 0) & op_value(proc, i, arena, 1));
-	return (0);
-}
-
-int					or(t_process proc, int source1, int source2, int *dest)
-{
-	if ((*dest = source1 | source2) == 0)
-		proc.carry = 1;
+	if ((*(int*)op_stock(proc, i, arena, 2) =
+		(op_value(proc, i, arena, 0) & op_value(proc, i, arena, 1))) == 0)
+		proc->carry = 1;
 	else
-		proc.carry = 0;
+		proc->carry = 0;
 	return (0);
 }
 
-int					xor(t_process proc, int source1, int source2, int *dest)
+int					or(t_process *proc, t_instruct *i, char *arena)
 {
-	if ((*dest = source1 ^ source2) == 0)
-		proc.carry = 1;
+	if ((*(int*)op_stock(proc, i, arena, 2) =
+		(op_value(proc, i, arena, 0) | op_value(proc, i, arena, 1))) == 0)
+		proc->carry = 1;
 	else
-		proc.carry = 0;
+		proc->carry = 0;
 	return (0);
 }
 
-int					zjmp(t_process proc, int source)
+int					xor(t_process *proc, t_instruct *i, char *arena)
 {
-	if (proc.carry == 1)
-		proc.pc = (proc.pc + source) % MEM_SIZE;
-	return (0);
-}
-
-int					ldi(t_process proc, int indirect_res, int *dest, char *arn)
-{
-	if ((*dest = *(int *)(&arn[(proc.pc + indirect_res) % MEM_SIZE])) == 0)
-		proc.carry = 1;
+	if ((*(int*)op_stock(proc, i, arena, 2) =
+		(op_value(proc, i, arena, 0) ^ op_value(proc, i, arena, 1))) == 0)
+		proc->carry = 1;
 	else
-		proc.carry = 0;
+		proc->carry = 0;
 	return (0);
 }
 
-int					sti(t_process proc, int *src, int indirect_res, char *arn)
+int					zjmp(t_process *proc, t_instruct *i, char *arena)
 {
-	if ((*(int *)(&arn[(proc.pc + indirect_res) % MEM_SIZE]) = *src) == 0)
-		proc.carry = 1;
-	else
-		proc.carry = 0;
+	if (proc->carry == 1)
+		proc->pc = (proc->pc + op_value(proc, i, arena, 0)) % MEM_SIZE;
 	return (0);
 }
