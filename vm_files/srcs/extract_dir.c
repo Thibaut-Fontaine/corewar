296a7420d8fbb1a9062961dc19bfd9093e8bbf98
Jeremy GAGNOT <jgagnot@e1r11p16.42.fr>
2017-07-11 18:10:05 +0200

    extract fixed

--- vm_files/srcs/extract_dir.c
+++ vm_files/srcs/extract_dir.c
@@ -6,7 +6,7 @@
 /*   By: jgagnot <jgagnot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/09 16:15:43 by jgagnot           #+#    #+#             */
-/*   Updated: 2017/06/28 17:53:04 by jgagnot          ###   ########.fr       */
+/*   Updated: 2017/07/11 17:31:16 by jgagnot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ int			extract_dir(t_process *proc, t_instruct *instruct, char *arena,
 	int *tab)
 {
 	int		size;
-	int		i;
+	unsigned int		i;
 
 	i = 0;
 	size = 0;
 	while (size < tab[3])
 	{
 		i = i << 8;
-		i = i | *(arena + ((proc->pc + instruct->size + size) % MEM_SIZE));
+		i = i | (*(arena + ((proc->pc + instruct->size + size) % MEM_SIZE))
+		& 0x000000FF);
 		size++;
 	}
 	instruct->size += tab[3];
--- vm_files/srcs/extract_ind.c
+++ vm_files/srcs/extract_ind.c
@@ -6,7 +6,7 @@
 /*   By: jgagnot <jgagnot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/09 16:15:51 by jgagnot           #+#    #+#             */
-/*   Updated: 2017/06/22 19:39:59 by mperronc         ###   ########.fr       */
+/*   Updated: 2017/07/11 17:32:20 by jgagnot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int			extract_ind(t_process *proc, t_instruct *instruct, char *arena)
 {
 	int		adr;
 
-	adr = 0 | *(arena + ((proc->pc + instruct->size) % MEM_SIZE)) * (1 << 8);
-	adr = adr | *(arena + ((proc->pc + instruct->size + 1) % MEM_SIZE));
+	adr = 0 | (*(arena + ((proc->pc + instruct->size) % MEM_SIZE)) * (1 << 8)
+	& 0x000000FF);
+	adr = adr | (*(arena + ((proc->pc + instruct->size + 1) % MEM_SIZE))
+	& 0x000000FF);
 	instruct->size += 2;
 	return (adr);
 }
