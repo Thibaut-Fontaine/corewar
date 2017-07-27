49ad515f4df15670d077233c711cb35e92f8c1c7
Matthias PERRONCEL <mperronc@student.42.fr>
2017-07-27 16:21:41 +0200

    norme run fight

--- vm_files/srcs/parse.c
+++ vm_files/srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <tfontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/28 20:35:26 by tfontain          #+#    #+#             */
-/*   Updated: 2017/07/26 21:53:03 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/07/27 16:12:31 by mperronc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,94 +62,8 @@ t_argv				*parse(int ac, const char *av[])
 	if (ret.f.flag & _G_)
 		ret.color = init_color_arena(&ret);
 	ret.ref_tab = get_ref_tab();
+	ret.cycle = 0;
+	ret.checks = 0;
+	ret.cycle_to_die = CYCLE_TO_DIE;
 	return (&ret);
 }
-
-// OLD:
-/*inline int		ret_flag(char c)
-{
-	if (c == 'g')
-		return (_G_);
-	else if (c == 's')
-		return (_S_);
-	else if (c == 'n')
-		return (_N_);
-	else if (c == 'd')
-		return (_D_);
-	else
-		return (0);
-}
-
-static inline int		fill_flag(t_opt *f, const char *c)
-{
-	if (c[0] == '-')
-	{
-		if (c[2] == 0)
-		{
-			f->flag |= ret_flag(c[1]);
-			return (ret_flag(c[1]));
-		}
-	}
-	return (0);
-}
-
-static inline int		get_arg(int i, t_opt *f, const char *argv[])
-{
-	if (i == _D_)
-		return (f->nd = ft_atoi(*argv));
-	else if (i == _S_)
-		return (f->ns = ft_atoi(*argv));
-	else if (i == _N_)
-		return (f->nn = ft_atoi(*argv));
-	else if (i == _G_)
-		return (0);
-	return (-1);
-}
-
-static inline char		*champion_to_memory(const char *arg,
-		t_header *current_champ, int n_players)
-{
-	char			*ptchamp;
-	char			*arena;
-	t_header		*tofree;
-
-	arena = generate_memory(&ptchamp, n_players);
-	tofree = open_file(arg, ptchamp);
-	*current_champ = *tofree;
-	free(tofree);
-	return (arena);
-}
-
-t_argv					*parse(int argc, const char *argv[])
-{
-	int				tmp;
-	static t_argv	ret;
-
-	ft_bzero(&ret, sizeof(ret));
-	--argc;
-	++argv;
-	ret.n_champs = count_champions(argc, argv);
-	if (ret.n_champs > MAX_PLAYERS)
-		error(_ERR_TOO_MANY_CH)();
-	ret.n_champs == 0 ? error(_ERR_USAGE)() : 0;
-	while (argc)
-	{
-		if (argc >= 2 && (tmp = fill_flag(&ret.f, *argv)) != 0)
-		{
-			if (get_arg(tmp, &ret.f, argv) != -1)
-			{
-				++argv;
-				--argc;
-			}
-		}
-		else
-			ret.arena = champion_to_memory(*argv, ret.champ + champ_num(C_),
-					ret.n_champs);
-		++argv;
-		--argc;
-	}
-	if (ret.f.flag & _G_)
-		ret.color = init_color_arena(&ret);
-	ret.ref_tab = get_ref_tab();
-	return (&ret);
-}*/
--- vm_files/srcs/run_fight.c
+++ vm_files/srcs/run_fight.c
@@ -6,42 +6,51 @@
 /*   By: tfontain <tfontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 18:46:49 by tfontain          #+#    #+#             */
-/*   Updated: 2017/07/27 14:00:23 by mperronc         ###   ########.fr       */
+/*   Updated: 2017/07/27 16:15:46 by mperronc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/vm.h"
+
+static void handle_dump(t_argv *info)
+{
+	if (is_there_flag(info->f, _S_) != -1 && info->f.n != 0
+			&& info->cycle % info->f.n == 0)
+		dump(info->arena);
+	else if (is_there_flag(info->f, _D_) != -1 &&
+			info->cycle == (unsigned int)info->f.n)
+		dump(info->arena);
+}
+
+static int handle_live(t_argv *info, t_plst *head)
+{
+	if (info->cycle % info->cycle_to_die == 0 && info->cycle)
+	{
+		++info->checks;
+		process_live(&head);
+		if (NBR_LIVE <= count_live(1) || info->checks % MAX_CHECKS == 0)
+		{
+			if ((info->cycle_to_die -= CYCLE_DELTA) <= 0)
+				return (0) ;
+			info->checks = 0;
+		}
+	}
+	return (1);
+}
 
 int				run(t_argv *info)
 {
 	t_plst			*head;
 
 	head = init_process(*info);
-	info->cycle = 0;
-	info->checks = 0;
-	info->cycle_to_die = CYCLE_TO_DIE;
 	if (is_there_flag(info->f, _G_) != -1)
 		gui(info, head);
 	while (head)
 	{
-		if (is_there_flag(info->f, _S_) != -1 && info->f.n != 0
-				&& info->cycle % info->f.n == 0)
-			dump(info->arena);
-		else if (is_there_flag(info->f, _D_) != -1 &&
-				info->cycle == (unsigned int)info->f.n)
-			dump(info->arena);
+		handle_dump(info);
 		execute_all_process(&head, info);
-		if (info->cycle % info->cycle_to_die == 0 && info->cycle)
-		{
-			++info->checks;
-			process_live(&head);
-			if (NBR_LIVE <= count_live(1) || info->checks % MAX_CHECKS == 0)
-			{
-				if ((info->cycle_to_die -= CYCLE_DELTA) <= 0)
-					break ;
-				info->checks = 0;
-			}
-		}
+		if (!handle_live(info, head))
+			break;
 		if (is_there_flag(info->f, _G_) != -1)
 		{
 			refresh_display(info, head);
